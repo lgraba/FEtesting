c6098e97caa9ee6b3a48c2cbd10bd77020060650
Logan Graba <lgraba@gmail.com>
2019-06-25 00:34:20 -0600

    Working on #1/#2

--- src/main.js
+++ src/main.js
@@ -1,77 +1,114 @@
-// const {Map} = require('immutable')
+const {List, Map} = require('immutable');
+const Immutable = require('immutable');
 
 // 1.
 let error = {
     name: ['This field is required', 'Another error'],
     age: ['Only numeric characters are allowed'],
-}
-// transformed goal
+};
+let immutableError = Immutable.fromJS(error);
+// Desired output
 // error = {
 //   name: 'This field is required. Another error.',
 //   age: 'Only numeric characters are allowed.'
 // }
 
-const flatten = ((obj, count = Object.keys(obj).length, result = {}) => {
-    if (count === 0) {
-        return result
+// Converting from and to Immutable Map
+const flatten = ((obj, result = {}) => {
+    obj = obj.toJS();
+
+    if (obj.size === 0) {
+        return obj
     }
-    for (let key in obj) {
+
+    for(let key in obj) {
         result[key] = obj[key].join('. ')
     }
-    return result
-    // flatten(error, count - 1, result)
-})
-flatten(error)
-console.log(flatten(error))
+
+    return Immutable.fromJS(result);
+});
+
+// console.log('#1: ', flatten(immutableError));
 
 // 2.
-// In case the errors are nested, you need to make sure that concatenated string won't have recurring errors. The nested structures could be both objects and arrays. The nested structures are not preserved, transformed object should have flat structure by default.
+// In case the errors are nested, you need to make sure that concatenated string
+//  won't have recurring errors. The nested structures could be both objects and
+//  arrays. The nested structures are not preserved, transformed object should
+//  have flat structure by default.
 
 let error2 = {
     name: {
         first: ['Only alphanumeric characters are allowed'],
         last: ['Only alphanumeric characters are allowed'],
     },
-    names: {} [{
+    names: [{
         first: ['Only alphanumeric characters are allowed'],
         last: ['Only alphanumeric characters are allowed'],
     }, {}],
-}
-// transformed
+};
+let immutableError2 = Immutable.fromJS(error2);
+// Desired output
 // error = {
 //   name: 'Only alphanumeric characters are allowed.',
 //   names: 'Only alphanumeric characters are allowed.',
 // }
 
-const nestedMerge = ((error, transform = {}) => {
-    if (error.name.first.toString() === error.name.last.toString()) {
-        transform.name = error.name.first.toString()
-        transform.names = error.name.first.toString()
-    }
-    return transform
-    // transform = Object.keys(error).reduce(function (r, k) {
-    //     return r.concat(k, error[k])
-    // }, [])
-    // console.log("result",transform)
-})
-nestedMerge(error2)
-console.log(nestedMerge(error2))
+// Map the given keys into a new object - there's probably a better way to do this ;-)
+const mapper = function mapper(keys, temp = {}) {
+    keys.map(
+        (value, index) => {
+            temp[value] = "";
+        }
+    );
 
+    return temp;
+};
+
+//
+// for(let key in obj) {
+//     result[key] = obj[key].join('. ')
+// }
+
+// Recursive nestedMerge
+const nestedMerge = ((root, parentKey, path = Immutable.List(), result = mapper(root.keySeq().toArray())) => {
+    if (!Immutable.isImmutable(root)) {
+        // Leaf Node - return what we were passed
+        return root;
+    }
+
+    for (const [key, child] of root.toKeyedSeq()) {
+        // If we don't have a result object, then we need to recurse and pass the Error value up
+        if (!result)
+            return nestedMerge(child, key, path.push(key), null);
+
+        // If we do have a result object, we're at the top of the hierarchy
+        result[key] = nestedMerge(child, key, path.push(key), null) + '.';
+    }
+
+    return Immutable.fromJS(result);
+});
+
+console.log('#2: ', nestedMerge(immutableError));
 
 // 3.
-// Sometimes, preserving nested structures could be useful when rendering errors on the screen. One of your implemented functions should take one or more arguments that specify the keys of error object for which you want to preserve the nested structure.
+// Sometimes, preserving nested structures could be useful when rendering errors
+//  on the screen. One of your implemented functions should take one or more
+//  arguments that specify the keys of error object for which you want to preserve
+//  the nested structure.
 //
-// For example, if you want to preserve nested structure for field `names`, the transformed object should look like:
+// For example, if you want to preserve nested structure for field `names`, the
+//  transformed object should look like:
 error3 = {
-  name: 'Only alphanumeric characters are allowed.',
-  names: [{}, {
-    first: 'Only alphanumeric characters are allowed.',
-    last: 'Only alphanumeric characters are allowed.',
-  }, {}],
+    name: 'Only alphanumeric characters are allowed.',
+    names: [{}, {
+        first: 'Only alphanumeric characters are allowed.',
+        last: 'Only alphanumeric characters are allowed.',
+    }, {}]
+};
 
+// I would use the same recursive function, but take out the
 
-  // const transformErrors = (obj)=>{
-  //     nestedMerge()
-  //     flatten()
-  // }
-  // transformErrors()
+exports._test = {
+    flatten: flatten,
+    nestedMerge: nestedMerge
+};
--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,7 @@
-const chai = require('chai')
-const expect = chai.expect
-const main = require('../src/main')
-const {Map} = require('immutable')
+const chai = require('chai');
+const expect = chai.expect;
+const main = require('../src/main');
+const {Map} = require('immutable');
 
 const Immutable = require('immutable');
 const assert = require('assert');
@@ -11,62 +11,78 @@ function transformErrors() {
     return Immutable.Map();
 }
 
-it('should tranform errors', () => {
-    // example error object returned from API converted to Immutable.Map
-    const errors = Immutable.fromJS({
-        name: ['This field is required'],
-        age: ['This field is required', 'Only numeric characters are allowed'],
-        urls: [{}, {}, {
-            site: {
-                code: ['This site code is invalid'],
-                id: ['Unsupported id'],
-            }
-        }],
-        url: {
-            site: {
-                code: ['This site code is invalid'],
-                id: ['Unsupported id'],
-            }
-        },
-        tags: [{}, {
-            non_field_errors: ['Only alphanumeric characters are allowed'],
-            another_error: ['Only alphanumeric characters are allowed'],
-            third_error: ['Third error']
-        }, {}, {
-            non_field_errors: [
-                'Minumum length of 10 characters is required',
-                'Only alphanumeric characters are allowed',
-            ],
-        }],
-        tag: {
-            nested: {
-                non_field_errors: ['Only alphanumeric characters are allowed'],
-            },
-        },
+
+describe('Testin Hard', () => {
+    it('Should make Logan happy!', () => {
+        const errors = Immutable.fromJS({
+            name: ['This field is required', 'You are too ugly to use this API']
+        });
+
+        const result = main._test.flatten(errors);
+
+        assert.deepEqual(result.toJS(), {
+            name: 'This field is required. You are too ugly to use this API'
+        });
+
     });
 
-    // in this specific case,
-    // errors for `url` and `urls` keys should be nested
-    // see expected object below
-    const result = transformErrors();
+    it('Should Transform Errors', () => {
+        // example error object returned from API converted to Immutable.Map
+        const errors = Immutable.fromJS({
+            name: ['This field is required'],
+            age: ['This field is required', 'Only numeric characters are allowed'],
+            urls: [{}, {}, {
+                site: {
+                    code: ['This site code is invalid'],
+                    id: ['Unsupported id'],
+                }
+            }],
+            url: {
+                site: {
+                    code: ['This site code is invalid'],
+                    id: ['Unsupported id'],
+                }
+            },
+            tags: [{}, {
+                non_field_errors: ['Only alphanumeric characters are allowed'],
+                another_error: ['Only alphanumeric characters are allowed'],
+                third_error: ['Third error']
+            }, {}, {
+                non_field_errors: [
+                    'Minumum length of 10 characters is required',
+                    'Only alphanumeric characters are allowed',
+                ],
+            }],
+            tag: {
+                nested: {
+                    non_field_errors: ['Only alphanumeric characters are allowed'],
+                },
+            },
+        });
 
-    assert.deepEqual(result.toJS(), {
-        name: 'This field is required.',
-        age: 'This field is required. Only numeric characters are allowed.',
-        urls: [{}, {}, {
-            site: {
-                code: 'This site code is invalid.',
-                id: 'Unsupported id.',
+        // in this specific case,
+        // errors for `url` and `urls` keys should be nested
+        // see expected object below
+        const result = main._test.nestedMerge(errors);
+
+        assert.deepEqual(result.toJS(), {
+            name: 'This field is required.',
+            age: 'This field is required. Only numeric characters are allowed.',
+            urls: [{}, {}, {
+                site: {
+                    code: 'This site code is invalid.',
+                    id: 'Unsupported id.',
+                },
+            }],
+            url: {
+                site: {
+                    code: 'This site code is invalid.',
+                    id: 'Unsupported id.',
+                },
             },
-        }],
-        url: {
-            site: {
-                code: 'This site code is invalid.',
-                id: 'Unsupported id.',
-            },
-        },
-        tags: 'Only alphanumeric characters are allowed. Third error. ' +
-            'Minumum length of 10 characters is required.',
-        tag: 'Only alphanumeric characters are allowed.',
+            tags: 'Only alphanumeric characters are allowed. Third error. ' +
+                'Minumum length of 10 characters is required.',
+            tag: 'Only alphanumeric characters are allowed.',
+        });
     });
 });
